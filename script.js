b1f42b0053c67531056275ebc445b03e81afac6b
DaveTrev <trevaskisdavid@gmail.com>
2024-04-18 21:16:11 +0000

    update loop added

--- script.js
+++ script.js
@@ -6,15 +6,30 @@ const worldElem = document.querySelector('[data-world]')
 setPixelToWorldScale()
 window.addEventListener("resize", setPixelToWorldScale)
 
+let lastTime
+function update(time) {
+    if (lastTime == null) {
+        lastTime = time
+        window.requestAnimationFrame(update)
+        return
+    }
+    const delta = time - lastTime
+    console.log(delta)
+
+    lastTime = time
+    window.requestAnimationFrame(update)
+}
+window.requestAnimationFrame(update)
+
+
 function setPixelToWorldScale() {
     let worldToPixelScale
-    if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT)
-    {
-        worldToPixelScale = window.innerWidth / WORLD_WIDTH
+    if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+      worldToPixelScale = window.innerWidth / WORLD_WIDTH
     } else {
-        worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+      worldToPixelScale = window.innerHeight / WORLD_HEIGHT
     }
-
-    worldElem.computedStyleMap.width = `${WORLD_WIDTH * worldToPixelScale}px`
-    worldElem.computedStyleMap.height = `${WORLD_HEIGHT * worldToPixelScale}px`
-}
+  
+    worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
+    worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+  }
